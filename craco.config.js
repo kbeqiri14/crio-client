d186686ddf804521392c81b9c2c3322426eafa60
Albert Navasardyan <albertnavasardyan@gmail.com>
2021-09-28 17:47:25 +0400

    craco config file added

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path');
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
+
+const themeVariables = lessToJs(
+  fs.readFileSync(path.join(__dirname, './src/antd-theme.less'), 'utf8'),
+);
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: themeVariables,
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
