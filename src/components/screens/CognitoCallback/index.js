21603e4e437d658b6d45e20449a05418d2116c09
Narine <narine@tidepoollabs.com>
2021-10-20 16:42:04 +0400

    fix(Profile): Remove backend requests

--- src/components/screens/CognitoCallback/index.js
+++ src/components/screens/CognitoCallback/index.js
@@ -13,38 +13,40 @@ import { GlobalSpinner } from '@ui-kit/GlobalSpinner';
 export const CognitoCallback = () => {
   const { access_token } = useQueryParams();
   const { user } = useCurrentUser();
-  useQuery(me, {
-    onCompleted: (data) => {
-      if (access_token && user) {
-        dispatchUser({
-          ...user?.attributes,
-          ...data?.me,
-          email: user?.attributes?.email,
-          username: `${user?.attributes?.firstName || user?.attributes?.given_name} ${user?.attributes?.lastName || user?.attributes?.family_name}`,
-        });
-        history.push(DEFAULT_PRIVATE_ROUTE);
-      }
-    },
-    onError: (data) => console.log(data, 'error'),
-  });
-  const [createUser] = useMutation(signIn);
+  // useQuery(me, {
+  //   onCompleted: (data) => {
+  //     if (access_token && user) {
+  //       dispatchUser({
+  //         ...user?.attributes,
+  //         ...data?.me,
+  //         email: user?.attributes?.email,
+  //         username: `${user?.attributes?.firstName || user?.attributes?.given_name} ${user?.attributes?.lastName || user?.attributes?.family_name}`,
+  //       });
+  //       history.push(DEFAULT_PRIVATE_ROUTE);
+  //     }
+  //   },
+  //   onError: (data) => console.log(data, 'error'),
+  // });
+  // const [createUser] = useMutation(signIn);
   const { dispatchUser } = useLoggedInUser();
 
   useEffect(() => {
     if (!access_token) {
       history.push('/');
+    } else {
+      history.push(DEFAULT_PRIVATE_ROUTE);
     }
   }, [access_token]);
 
-  useEffect(() => {
-    if (access_token && user) {
-      try {
-        createUser().then();
-      } catch (e) {
-        console.log('error creating user ', e);
-      }
-    }
-  }, [access_token, user, createUser]);
+  // useEffect(() => {
+  //   if (access_token && user) {
+  //     try {
+  //       // createUser().then();
+  //     } catch (e) {
+  //       console.log('error creating user ', e);
+  //     }
+  //   }
+  // }, [access_token, user]);
 
   return <GlobalSpinner />;
 };
