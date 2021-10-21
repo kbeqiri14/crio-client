62d3bd2215b517ea5426b548ee1506a3d817ef9f
Narine <narine@tidepoollabs.com>
2021-10-21 12:00:16 +0400

    feat(Profile): Restructured profile folders

---
+++ src/components/screens/Profiles/__partials__/EditProfile/Footer.js
@@ -0,0 +1,48 @@
+import { memo, useCallback } from 'react';
+import { Col, Row } from 'antd';
+import { useMutation } from '@apollo/client';
+
+import { useLoggedInUser } from '@app/hooks/useLoggedInUser';
+import { updateUser } from '@app/graphql/mutations/user.mutation';
+import { successToast } from '@ui-kit/Notification';
+import { SecondaryButton } from '@ui-kit/Button';
+
+const Footer = ({ user, closeModal, handleSubmit }) => {
+  const [updateUserInfo, { loading }] = useMutation(updateUser, {
+    onCompleted: (data) => {
+      dispatchUser({ ...user, ...data.updateUser });
+      closeModal();
+      successToast('Your profile has been updated.')
+    },
+  });
+  const { dispatchUser } = useLoggedInUser();
+
+  const onSubmit = useCallback(
+    (attributes) => updateUserInfo({ variables: { attributes } }),
+    [updateUserInfo],
+  );
+
+  return <Col>
+    <Row gutter={30}>
+      <Col>
+        <SecondaryButton textColor='white_75' borderColor='white_75' size='large' onClick={closeModal}>
+          CANCEL
+        </SecondaryButton>
+      </Col>
+      <Col>
+        <SecondaryButton
+          filled
+          fillColor='white_25'
+          textColor='white_75'
+          size='large'
+          loading={loading}
+          onClick={handleSubmit(onSubmit)}
+        >
+          SAVE
+        </SecondaryButton>
+      </Col>
+    </Row>
+  </Col>
+};
+
+export default memo(Footer);
--- src/components/screens/Profiles/__partials__/EditProfile/index.js
+++ src/components/screens/Profiles/__partials__/EditProfile/index.js
@@ -1,102 +1,14 @@
-import { memo, useCallback } from 'react';
+import { memo } from 'react';
 import { Col, Modal, Row } from 'antd';
-import { Controller, useForm } from 'react-hook-form';
-import { useMutation } from '@apollo/client';
+import { useForm } from 'react-hook-form';
 
-import { useLoggedInUser } from '@app/hooks/useLoggedInUser';
-import { updateUser } from '@app/graphql/mutations/user.mutation';
-import { Title } from '@ui-kit/Text';
-import { Input } from '@ui-kit/Input';
-import { SecondaryButton } from '@ui-kit/Button';
-import { successToast } from '@ui-kit/Notification';
 import { ReactComponent as CloseIcon } from '@svgs/close.svg';
-import { ReactComponent as PublicIcon } from '@svgs/public.svg';
-import { ReactComponent as PrivateIcon } from '@svgs/private.svg';
-import Visibility from './Visibility';
+import { FormRow, Header, Item } from './partials';
+import Footer from './Footer';
 import './styles.less';
-
-const menuItems = [
-  {
-    title: 'Public',
-    value: 'public',
-    icon: <PublicIcon />,
-  },
-  {
-    title: 'Only me',
-    value: 'only_me',
-    icon: <PrivateIcon />,
-  },
-];
-
-const Header = () => (
-  <Col span={24}>
-    <Title level={10} color='white'>
-      Edit Profile
-    </Title>
-  </Col>
-);
-
-const Footer = ({ loading, disabled, onCancel, onSave }) => (
-  <Col>
-    <Row gutter={30}>
-      <Col>
-        <SecondaryButton textColor='white_75' borderColor='white_75' size='large' onClick={onCancel}>
-          CANCEL
-        </SecondaryButton>
-      </Col>
-      <Col>
-        <SecondaryButton filled fillColor='white_25' textColor='white_75' size='large' loading={loading} onClick={onSave}>
-          SAVE
-        </SecondaryButton>
-      </Col>
-    </Row>
-  </Col>
-);
-
-const Item = ({ text, span, control, name, disabled, defaultValue }) => (
-  <Col span={span}>
-    <Row gutter={[0, 10]}>
-      <Col span={24}>
-        <Title inline level={30} color='white'>
-          {text}
-        </Title>
-      </Col>
-      <Col span={24}>
-        <Controller
-          name={name}
-          control={control}
-          defaultValue={defaultValue}
-          render={({ field }) => <Input {...field} disabled={disabled} />}
-        />
-      </Col>
-    </Row>
-  </Col>
-);
-
-const FormRow = ({ children }) => (
-  <Col span={24}>
-    <Row justify='center' align='bottom' gutter={20}>
-      {children}
-      <Visibility options={menuItems} onChange={undefined} />
-    </Row>
-  </Col>
-);
 
 const EditProfile = ({ user, visible, closeModal }) => {
   const { control, handleSubmit } = useForm();
-  const [updateUserInfo, { loading }] = useMutation(updateUser, {
-    onCompleted: (data) => {
-      dispatchUser({ ...user, ...data.updateUser });
-      closeModal();
-      successToast('Your profile has been updated.')
-    },
-  });
-  const { dispatchUser } = useLoggedInUser();
-
-  const onSubmit = useCallback(
-    (attributes) => updateUserInfo({ variables: { attributes } }),
-    [updateUserInfo],
-  );
 
   return (
     <Modal
@@ -113,42 +25,42 @@ const EditProfile = ({ user, visible, closeModal }) => {
           <Row gutter={[50, 32]}>
             <FormRow>
               <Item
+                name='firstName'
                 text='First name'
                 span={8}
                 control={control}
-                name='firstName'
                 defaultValue={user?.firstName || user?.given_name}
               />
               <Item
+                name='lastName'
                 text='Last name'
                 span={9}
                 control={control}
-                name='lastName'
                 defaultValue={user?.lastName || user?.family_name}
               />
             </FormRow>
             <FormRow>
               <Item
+                name='username'
                 text='Username *'
                 span={17}
                 control={control}
-                name='username'
                 defaultValue={user?.username || `${user?.firstName || user?.given_name} ${user?.lastName || user?.family_name}`}
-          />
+              />
             </FormRow>
             <FormRow>
               <Item
+                name='email'
                 text='Email'
                 span={17}
                 control={control}
-                name='email'
                 defaultValue={user?.email}
                 disabled
               />
             </FormRow>
           </Row>
         </Col>
-        <Footer loading={loading} onCancel={closeModal} onSave={handleSubmit(onSubmit)} />
+        <Footer user={user} closeModal={closeModal} handleSubmit={handleSubmit} />
       </Row>
     </Modal>
   );
