583ad716d30d3703137d46c3e04decb344c74125
VanTigranyan <lostfank@gmail.com>
2021-11-10 16:30:09 +0400

    fix(Profile): Correct works grid

--- src/components/screens/Upload/CoverImage.js
+++ src/components/screens/Upload/CoverImage.js
@@ -17,9 +17,13 @@ const CoverImage = ({ visible, artworkId }) => {
   const [updateArtwork, { loading: updatingArtwork }] = useMutation(updateMetadata, {
     onCompleted: () => history.push('/account'),
   });
-  const onCancel = useCallback(() => updateArtwork({
-    variables: { params: { artworkId } },
-  }), [artworkId, updateArtwork]);
+  const onCancel = useCallback(
+    () =>
+      updateArtwork({
+        variables: { params: { artworkId } },
+      }),
+    [artworkId, updateArtwork],
+  );
 
   const props = {
     name: 'file',
@@ -29,12 +33,12 @@ const CoverImage = ({ visible, artworkId }) => {
     listType: 'picture',
     beforeUpload(file) {
       const getSource = async () => {
-        const src = await new Promise(resolve => {
+        const src = await new Promise((resolve) => {
           const reader = new FileReader();
           reader.readAsDataURL(file);
           reader.onload = () => resolve(reader.result);
         });
-        setSource(src)
+        setSource(src);
       };
       getSource();
     },
@@ -42,29 +46,43 @@ const CoverImage = ({ visible, artworkId }) => {
 
   return (
     <BlurredModal blurred maskClosable={false} visible={visible} width={686} onCancel={onCancel}>
-      <Row justify='center'gutter={[0, 40]} className='cover-image'>
+      <Row justify='center' gutter={[0, 40]} className='cover-image'>
         <Col span={24}>
-          <Title inline level='10' color='white'>Upload cover image</Title>
+          <Title inline level='10' color='white'>
+            Upload cover image
+          </Title>
         </Col>
         <Col span={24} className='desc'>
-          <Text inline level='10' color='white'>If skipped we will generate a cover image from video.</Text>
+          <Text inline level='10' color='white'>
+            If skipped we will generate a cover image from video.
+          </Text>
         </Col>
         <Col span={24}>
-          {source
-            ? <img alt='uploaded file' src={source} className='uploaded-image' />
-            : <Dragger {...props}>
+          {source ? (
+            <img alt='uploaded file' src={source} className='uploaded-image' />
+          ) : (
+            <Dragger {...props}>
               <Row justify='center' align='center' gutter={[0, 11]} className='drag-and-drop'>
                 <Col span={24}>
                   <img alt='cover' src={coverImage} />
                 </Col>
                 <Col span={24}>
-                  <Text inline level='10' color='white'>Drag and drop an image, or <a>Upload</a></Text>
+                  <Text inline level='10' color='white'>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    Drag and drop an image, or <a>Upload</a>
+                  </Text>
                 </Col>
               </Row>
-            </Dragger>}
+            </Dragger>
+          )}
         </Col>
         <Col span={24}>
-          <ActionButtons cancelText='SKIP' saveText='PUBLISH' onCancel={onCancel} onSave={onCancel} />
+          <ActionButtons
+            cancelText='SKIP'
+            saveText='PUBLISH'
+            onCancel={onCancel}
+            onSave={onCancel}
+          />
         </Col>
       </Row>
     </BlurredModal>
