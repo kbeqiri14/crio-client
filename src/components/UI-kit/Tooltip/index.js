8361221e018680702df0f9704bed1b7c75543d58
Narine <narine@tidepoollabs.com>
2021-11-11 15:44:48 +0400

    feat(UI-KIT): Added custom tooltip

---
+++ src/components/UI-kit/Tooltip/index.js
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+import { Tooltip } from 'antd';
+
+import { Text, Title } from '@ui-kit/Text';
+import './styles.less';
+
+export const CustomTooltip = ({ children, visible, title, description, className, ...props }) => (
+  <Tooltip
+    {...props}
+    overlayClassName={className}
+    visible={visible}
+    color='rgba(112, 114, 128, 1)'
+    title={
+      <Fragment>
+        {title && <Title level={30} color='white'>
+          {title}
+        </Title>}
+        <Text level={20} color='white'>
+          {description}
+        </Text>
+      </Fragment>
+    }
+  >
+    {children}
+  </Tooltip>
+);
