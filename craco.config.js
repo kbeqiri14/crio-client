cd8adb05ba11322fe30fca9c8efedcefd839b22f
VanTigranyan <van@tidepoollabs.com>
2021-11-15 17:02:19 +0400

    feat(App): Disable caching for the dev server

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,11 @@ const CracoAntDesignPlugin = require('craco-antd');
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+  devServer: {
+    headers: {
+      'Cache-Control': 'no-cache',
+    }
+  },
   plugins: [
     {
       plugin: CracoAntDesignPlugin,
