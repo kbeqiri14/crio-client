6b5937484f7dc6850f92da01f8d42e79465a462a
Narine <narine@tidepoollabs.com>
2021-11-30 14:18:38 +0400

    fix(App): Fixed loop issue

--- src/hooks/useRandomArtworks.js
+++ src/hooks/useRandomArtworks.js
@@ -21,7 +21,7 @@ export const useRandomArtworks = (onCompleted, offset = 0, limit = LIMIT) => {
 
   const { data: artworksCount } = useQuery(getRandomArtworksCount, {
     onCompleted: ({ getRandomArtworksCount }) => {
-      if (getRandomArtworksCount >= 8) {
+      if (getRandomArtworksCount >= 15 && limit === LIMIT) {
         const n = Math.floor(Math.random() * getRandomArtworksCount + 1);
         randomNumberVar(n);
         requestRandomArtworks({
