480419343d35ace7725dc4b86e562f7f28c77a20
Narine <narine@tidepoollabs.com>
2022-05-31 16:59:50 +0400

    fix(Product): Make private Upload url

--- src/components/screens/UploadProduct/index.js
+++ src/components/screens/UploadProduct/index.js
@@ -1,8 +1,18 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 
+import history from '@app/configs/history';
+import { useLoggedInUser } from '@app/hooks/useLoggedInUser';
 import ProductDetails from '@root/src/components/screens/EditProduct/ProductForm';
 
 const UploadProduct = () => {
+  const { user } = useLoggedInUser();
+
+  useEffect(() => {
+    if (!user.isCreator) {
+      history.push('/');
+    }
+  }, [user.isCreator]);
+
   return <ProductDetails />;
 };
 
