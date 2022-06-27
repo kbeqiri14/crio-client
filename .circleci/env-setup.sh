bb2800d08e72fe6bef43f734aea94c7b38e5c012
Narine <narine@tidepoollabs.com>
2022-06-27 12:49:27 +0400

    feat(Stripe): Update STRIPE_PAYMENT_URL

--- .circleci/env-setup.sh
+++ .circleci/env-setup.sh
@@ -47,7 +47,7 @@ then
     echo "export REACT_APP_COGNITO_APP_CLIENT_ID=2gj4elpucbicbuc8eqbfdvnft9"
     echo "export REACT_APP_COGNITO_USER_POOL_ID=us-east-1_hY3WdkmUI"
     echo "export REACT_APP_COGNITO_DOMAIN=crio-in-production-auth.auth.us-east-1.amazoncognito.com"
-    echo "export REACT_APP_STRIPE_PAYMENT_URL=https://buy.stripe.com/4gw4ib1fXggn0KcdQQ"
+    echo "export REACT_APP_STRIPE_PAYMENT_URL=https://buy.stripe.com/6oE5mf6Ahe8ffF6dQR"
     echo "export REACT_APP_GTAG=UA-215590535-1"
     echo "export REACT_APP_BUCKET_NAME=crio-in-production-bucket"
   } >> "$BASH_ENV"
