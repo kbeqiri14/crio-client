5098d161cd058107f86b143e3c58c7eb0f4314ea
ArturKojoyan <ArturKojoyan15@mail.ru>
2022-07-08 12:40:47 +0400

    fix(ExplorePage): Add no result icon

---
+++ src/components/UI-kit/General/Typography/Paragraph.js
@@ -0,0 +1,25 @@
+import { Typography } from 'antd';
+import styled, { css } from 'styled-components';
+
+const Paragraph = styled(Typography.Paragraph)`
+  color: ${(props) => props.theme.colors.white};
+  &.ant-typography-disabled {
+    color: ${(props) => props.theme.colors.dark50};
+  }
+  ${(props) =>
+    props?.level &&
+    css`
+      font-size: ${props.theme.text[props.level].size}px;
+      font-weight: ${props.theme.text[props.level].weight};
+      line-height: ${props.theme.text[props.level].height}px;
+      font-style: ${props.theme.text[props.level].style || 'normal'};
+    `}
+
+  ${(props) =>
+    props?.color &&
+    css`
+      color: ${props.theme.colors[props.color]};
+    `}
+`;
+
+export default Paragraph;
