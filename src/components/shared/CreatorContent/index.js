9feb4bea27abba982ff1fe3b995debca667bd9a2
ArturKojoyan <ArturKojoyan15@mail.ru>
2022-07-08 12:54:56 +0400

    fix(ExplorePage): Add no data found icon

--- src/components/shared/CreatorContent/index.js
+++ src/components/shared/CreatorContent/index.js
@@ -2,14 +2,23 @@ import { memo, useCallback, useMemo, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { Tabs } from '@ui-kit';
+import { Tabs, Paragraph } from '@ui-kit';
 import LoadMoreButton from './LoadMoreButton';
 import EmptyState from '@shared/EmptyState';
 import ArtworksList from './Artwork/ArtworksList';
 import ProductsList from './Product/ProductsList';
+import { ReactComponent as EmptyMarketIcon } from '@svgs/marketplace-empty.svg';
+import { ReactComponent as EmptyArtworkIcon } from '@svgs/artwork-empty.svg';
 
 const Wrapper = styled('div')`
   max-width: 1438px;
+  margin: auto;
+  padding: 40px 22px;
+
+  .empty {
+    text-align: center;
+  }
+
   @media (max-width: 1437px) {
     max-width: 1084px;
   }
@@ -19,8 +28,6 @@ const Wrapper = styled('div')`
   @media (max-width: 729px) {
     max-width: 376px;
   }
-  margin: auto;
-  padding: 40px 22px;
 `;
 
 const { TabPane } = Tabs;
@@ -82,6 +89,17 @@ export const Content = ({
               isMarketplace={true}
             />
           )}
+
+          {!productsList.length && (
+            <div className='empty'>
+              <EmptyMarketIcon />
+
+              <Paragraph level={4} color='dark25'>
+                no result
+              </Paragraph>
+            </div>
+          )}
+
           <ProductsList productsList={productsList} />
           <LoadMoreButton
             visible={visibleLoadMoreProducts}
@@ -93,6 +111,17 @@ export const Content = ({
           {isProfilePage && !artworksCount && !artworksList?.length && (
             <EmptyState username={username} isCreator={true} isProfile={isProfile} />
           )}
+
+          {!artworksList.length && (
+            <div className='empty'>
+              <EmptyArtworkIcon />
+
+              <Paragraph level={4} color='dark25'>
+                No Data found
+              </Paragraph>
+            </div>
+          )}
+
           <ArtworksList artworksList={artworksList} />
           <LoadMoreButton
             visible={visibleLoadMoreArtworks}
