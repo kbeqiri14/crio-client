0fe66fcc136ceef9404c9c00ee56fdfd05bd9faa
ArturKojoyan <ArturKojoyan15@mail.ru>
2022-07-12 16:26:18 +0400

    Add time for product delete notification visibility

--- src/components/screens/Video/Actions.js
+++ src/components/screens/Video/Actions.js
@@ -21,7 +21,7 @@ const Actions = (props) => {
     onCompleted: () => {
       hideConfirmation();
       successToast('The product is successfully deleted');
-      window.location.href = `/profile/${props.username}`;
+      setTimeout(() => (window.location.href = `/profile/${props.username}`), 1300);
     },
   });
 
@@ -30,7 +30,7 @@ const Actions = (props) => {
     onCompleted: () => {
       hideConfirmation();
       successToast('The video is successfully deleted');
-      window.location.href = `/profile/artworks/${props.username}`;
+      setTimeout(() => (window.location.href = `/profile/artworks/${props.username}`));
     },
   });
 
