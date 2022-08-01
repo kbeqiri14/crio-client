1da116a16409331aa290bf3ec1057d71205d6d1b
Artur <ArturKojoyan15@mail.ru>
2022-08-01 15:11:39 +0400

    fix(Content): Remove useless classes

--- src/components/screens/Artwork/Content.js
+++ src/components/screens/Artwork/Content.js
@@ -108,7 +108,7 @@ export const Content = ({ info, content, isLocked }) => {
           </Row>
         </Col>
         {isLocked ? (
-          <Col className='image-padding' span={24}>
+          <Col span={24}>
             <div className='lock'>
               <LockState userId={info.userId} accessibility={info.accessibility} size='lg' />
               <ImageWrapper>
@@ -121,7 +121,7 @@ export const Content = ({ info, content, isLocked }) => {
             </div>
           </Col>
         ) : (
-          <Col className='image-padding' span={24}>
+          <Col span={24}>
             {info.isProduct || info.isImage ? (
               <ImageWrapper>
                 <img
