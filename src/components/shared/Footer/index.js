b46217d3864b3e861b44fd388adecabebd0da898
Artur <ArturKojoyan15@mail.ru>
2022-09-06 21:32:15 +0400

    fix(Footer): Change footer mobile view

--- src/components/shared/Footer/index.js
+++ src/components/shared/Footer/index.js
@@ -12,6 +12,12 @@ const links = [
 ];
 
 const Wrapper = styled('div')`
+  @media (max-width: 768px) {
+    margin-right: 45px;
+    .flex-direction {
+      flex-direction: column;
+    }
+  }
   @media (max-width: 915px) {
     .container {
       flex-direction: column-reverse;
@@ -26,7 +32,7 @@ const Footer = () => (
       padding_vertical={20}
       className='container items-center'
     >
-      <Col>
+      <Col lg={8}>
         <Row justify='center' align='center' gutter={[20]} className='items-center'>
           <Col>
             <img alt='Crio logo' src={logo} width={48} height={28} />
@@ -36,25 +42,43 @@ const Footer = () => (
           </Col>
         </Row>
       </Col>
-      <Col>
-        <Row justify='center' gutter={[20, 20]}>
-          <Col key={'FAQ'}>
-            <Link to={'/faq'}>
-              <Text level={3}>FAQ</Text>
-            </Link>
+      <Col lg={10}>
+        <Row justify='center' gutter={[35, 20]}>
+          <Col lg={0} md={0}>
+            <Row className='flex-direction' gutter={[0, 20]}>
+              <Col key={'FAQ'}>
+                <Link to={'/faq'}>
+                  <Text level={3}>FAQ</Text>
+                </Link>
+              </Col>
+              <Col>
+                <a href={`mailto:info@criointeractive.com`}>
+                  <Text level={3}>Contact Us</Text>
+                </a>
+              </Col>
+            </Row>
           </Col>
-          <Col>
-            <a href={`mailto:info@criointeractive.com`}>
-              <Text level={3}>Contact Us</Text>
-            </a>
+          <Col lg={24}>
+            <Row className='flex-direction' gutter={[20, 20]}>
+              <Col xxl={2} xl={3} lg={3} md={3} sm={0} xs={0} key={'FAQ'}>
+                <Link to={'/faq'}>
+                  <Text level={3}>FAQ</Text>
+                </Link>
+              </Col>
+              <Col xxl={4} xl={6} lg={6} md={6} sm={0} xs={0}>
+                <a href={`mailto:info@criointeractive.com`}>
+                  <Text level={3}>Contact Us</Text>
+                </a>
+              </Col>
+              {links.map(({ to, label }) => (
+                <Col key={label}>
+                  <Link to={to}>
+                    <Text level={3}>{label}</Text>
+                  </Link>
+                </Col>
+              ))}
+            </Row>
           </Col>
-          {links.map(({ to, label }) => (
-            <Col key={label}>
-              <Link to={to}>
-                <Text level={3}>{label}</Text>
-              </Link>
-            </Col>
-          ))}
         </Row>
       </Col>
     </Row>
