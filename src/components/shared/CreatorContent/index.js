16c989359632688ebcae0dcdcdcfe32a3b844935
Artur <arturkojoyan15@mail.ru>
2022-09-16 17:10:39 +0400

    feat(ExplorePage): Add new product categories

--- src/components/shared/CreatorContent/index.js
+++ src/components/shared/CreatorContent/index.js
@@ -3,11 +3,12 @@ import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 import history from '@configs/history';
-import { Tabs } from '@ui-kit';
+import { Col, Row, Tabs } from '@ui-kit';
 import EmptyState from '@shared/EmptyState';
 import LoadMoreButton from './LoadMoreButton';
 import ArtworksList from './Artwork/ArtworksList';
 import ProductsList from './Product/ProductsList';
+import TagButton from '@ui-kit/Custom/TagButton';
 
 const Wrapper = styled('div')`
   max-width: 1438px;
@@ -31,6 +32,19 @@ const tabs = {
   MARKETPLACE: 'Marketplace',
   ARTWORK: 'Content',
 };
+
+const productTabs = [
+  { name: 'eBook', color: '#00A0FF', width: '75' },
+  { name: 'Comics', color: '#CF04A3', width: '78' },
+  { name: 'Artwork', color: '#A304CB', width: '78' },
+  { name: 'Art Tools % Assets', color: '#4C9A08', width: '150' },
+  { name: 'Game Assets', color: '#00A0FF', width: '118' },
+  { name: 'Software', color: '#CF04A3', width: '87' },
+  { name: 'Photos', color: '#A304CB', width: '72' },
+  { name: 'Video', color: '#4C9A08', width: '65' },
+  { name: 'Guids/ Documents', color: '#00A0FF', width: '157' },
+  { name: 'Tamplates', color: '#CF04A3', width: '100' },
+];
 
 export const Content = ({
   isProfile,
@@ -76,6 +90,17 @@ export const Content = ({
     <Wrapper>
       <Tabs activeKey={activeKey} onTabClick={onTabClick}>
         <TabPane key={tabs.MARKETPLACE} tab={tabs.MARKETPLACE}>
+          {!isProfilePage && (
+            <Row gutter={[12, 12]} padding_bottom={20}>
+              {productTabs.map((item) => (
+                <Col>
+                  <TagButton b_color={item.color} width={item.width}>
+                    {item.name}
+                  </TagButton>
+                </Col>
+              ))}
+            </Row>
+          )}
           {!loading && !productsCount && !productsList?.length && (
             <EmptyState {...props} isMarketplace={true} />
           )}
