dec8307b61b4e62d159828f94d8d4d76ca1e41ce
Artur <arturkojoyan15@mail.ru>
2022-10-03 21:12:27 +0400

    feat(TopProducts): Add ability to click on product

--- src/components/screens/ExplorePage/TopProducts.js
+++ src/components/screens/ExplorePage/TopProducts.js
@@ -1,19 +1,19 @@
 import { memo, useCallback, useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { signIn } from '@app/auth';
 import { useLoggedInUser } from '@app/hooks/useLoggedInUser';
+import { usePresentation } from '@shared/PresentationView/PresentationContext';
 import useAvatarUrl from '@app/hooks/useAvatarUrl';
 import { PRODUCTS } from '@configs/constants';
 import { getThumbnail } from '@utils/helpers';
+import history from '@configs/history';
 import { Button, Col, Row, Text, Title } from '@ui-kit';
 import { ReactComponent as GoogleIcon } from '@svgs/google.svg';
 
 const Wrapper = styled('div')`
-  height: 450px;
   padding-top: 85px;
-
   @media (max-width: 767.98px) {
     .title {
       font-size: 35px !important;
@@ -26,6 +26,7 @@ const ProductWrapper = styled('div')`
   width: 304px;
   background: #202020;
   border-radius: 14px;
+  cursor: pointer;
   .img {
     border-top-left-radius: 14px;
     border-top-right-radius: 14px;
@@ -38,33 +39,83 @@ const ProductWrapper = styled('div')`
 
 const TopProducts = ({
   userId,
-  username,
   providerType,
   providerUserId,
+  username,
   avatar,
-  thumbnail,
+  productId,
+  categoryId,
   title,
+  description,
   price,
+  limit,
+  accessibility,
+  thumbnail,
+  file,
 }) => {
   const { user } = useLoggedInUser();
   const googleSignIn = useCallback(() => signIn('Google'), []);
+  const { pathname } = useLocation();
+  const { setInfo } = usePresentation();
   const avatarUrl = useAvatarUrl(providerType, providerUserId, avatar);
   const src = useMemo(
     () => getThumbnail(PRODUCTS, userId, `thumbnail-${thumbnail}`),
     [userId, thumbnail],
   );
 
+  const showProduct = useCallback(() => {
+    if (pathname.includes('/product/')) {
+      history.push(`/product/${productId}`);
+      return;
+    }
+    window.history.replaceState('', '', `/product/${productId}`);
+    setInfo({
+      userId,
+      providerType,
+      providerUserId,
+      username,
+      avatar,
+      productId,
+      categoryId,
+      title,
+      description,
+      price,
+      limit,
+      accessibility,
+      thumbnail: src,
+      file,
+      isProduct: true,
+    });
+  }, [
+    userId,
+    providerType,
+    providerUserId,
+    username,
+    avatar,
+    categoryId,
+    title,
+    description,
+    price,
+    limit,
+    accessibility,
+    src,
+    file,
+    pathname,
+    productId,
+    setInfo,
+  ]);
+
   return (
     <Wrapper>
-      <Row justify='space-around' gutter={[0, 40]}>
+      <Row justify='space-around' gutter={[0, 50]}>
         <Col
           padding_top={25}
           xxl={{ span: 10, offset: 2 }}
           xl={{ span: 12, offset: 0 }}
           lg={{ span: 18, offset: 2 }}
-          md={{ span: 14, offset: 0 }}
-          sm={{ span: 14, offset: 0 }}
-          xs={{ span: 14, offset: 0 }}
+          md={{ span: 20, offset: 2 }}
+          sm={{ span: 20, offset: 1 }}
+          xs={{ span: 20, offset: 0 }}
         >
           <Row gutter={[0, 20]}>
             <Col span={24}>
@@ -94,9 +145,10 @@ const TopProducts = ({
             )}
           </Row>
         </Col>
-        <Col xl={6} xs={12}>
+
+        <Col xl={6} sm={8} md={6} lg={8} xs={18}>
           <Row>
-            <ProductWrapper>
+            <ProductWrapper onClick={showProduct}>
               <Col span={24}>
                 <img alt='artwork' height={304} width={284} className='img fit-cover' src={src} />
               </Col>
@@ -109,18 +161,22 @@ const TopProducts = ({
           </Row>
           <Row padding_top={7}>
             <Col span={2}>
-              <img
-                src={avatarUrl}
-                height='33'
-                width='33'
-                alt='Author avatar'
-                className='border-radius-100'
-              />
+              <Link to={`/profile/${username}`}>
+                <img
+                  src={avatarUrl}
+                  height='33'
+                  width='33'
+                  alt='Author avatar'
+                  className='border-radius-100'
+                />
+              </Link>
             </Col>
             <Col span={10} className='self-center'>
-              <Text color='dark50' level={3}>
-                {username}
-              </Text>{' '}
+              <Link to={`/profile/${username}`}>
+                <Text color='dark50' level={3}>
+                  {username}
+                </Text>{' '}
+              </Link>
             </Col>
             {price && (
               <Col span={2} offset={2} className='self-center'>
