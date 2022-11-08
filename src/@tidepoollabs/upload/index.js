75e72cf74360a5859d6d1c0d17fcc15a188e9427
Narine <narine@tidepoollabs.com>
2022-11-08 13:39:40 +0400

    fix deploy

---
+++ src/@tidepoollabs/upload/index.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+import axios from 'axios';
+
+export const s3SignMutation = gql`
+  mutation ($filename: String!, $filetype: String!) {
+    signS3(filename: $filename, filetype: $filetype) {
+      url
+      signedRequest
+    }
+  }
+`;
+
+export const uploadToS3 = async (file, signedRequest) => {
+  console.log('Narine');
+  const options = {
+    headers: {
+      'Content-Type': file.type,
+    },
+  };
+  await axios.put(signedRequest, file, options);
+};
+
+export default class Upload {
+  constructor(graphqlClient) {
+    this.client = graphqlClient;
+  }
+
+  async sign(filename, filetype) {
+    const {
+      data: { signS3 },
+    } = await this.client.mutate({
+      mutation: s3SignMutation,
+      variables: {
+        filename,
+        filetype,
+      },
+    });
+    return {
+      url: signS3.url,
+      signedRequest: signS3.signedRequest,
+    };
+  }
+
+  async signAndUpload(filename, filetype, file) {
+    const { url, signedRequest } = await this.sign(filename, filetype);
+    await uploadToS3(file, signedRequest);
+    return url;
+  }
+}
