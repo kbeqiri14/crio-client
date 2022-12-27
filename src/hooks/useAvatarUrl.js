f580242f06f4a2e0f692e187a141ec62a6ac8e10
Narine <narine@tidepoollabs.com>
2022-12-27 12:28:13 +0400

    fix region

--- src/hooks/useAvatarUrl.js
+++ src/hooks/useAvatarUrl.js
@@ -1,9 +1,9 @@
-import { BUCKET_NAME } from '@app/configs/environment';
+import { BUCKET_NAME, COGNITO_REGION } from '@app/configs/environment';
 import defaultAvatar from '@svgs/avatar.svg';
 
 const useAvatarUrl = (userId, image) =>
   image
-    ? `https://${BUCKET_NAME}.s3.us-west-2.amazonaws.com/${userId}/profile/${image}`
+    ? `https://${BUCKET_NAME}.s3.${COGNITO_REGION}.amazonaws.com/${userId}/profile/${image}`
     : defaultAvatar;
 
 export default useAvatarUrl;
